486d5088bc82ccb0f736e6979709112390f5c6fd
Venki <venkitesh.r.mallan@gmail.com>
2023-03-06 15:12:36 +0530

    Enhanced to Support Nested Columns

--- ssisParse.py
+++ ssisParse.py
@@ -4,6 +4,8 @@ import os,json
 import pandas as pd
 import yaml
 from typing import Dict
+import flatdict
+import re,csv
 
 def loadParserConfig():
     config_path = os.path.join(os.path.realpath(os.path.dirname(__file__)),"config.yml")
@@ -41,17 +43,42 @@ def extractModelMeta(configuration, modelname):
     return modelMeta
 
 def getModelDataAsPd(data, modelMeta):
-    parentPath =  cleanKey(modelMeta.get("parent"))
-    parentData = data.get(parentPath)
-    if not isinstance(parentData,list):
-        parentData = [parentData]
+    #parentPath =  cleanKey(modelMeta.get("parent"))
+    #parentData = data.get(parentPath)
+    if not isinstance(data,list):
+        data = [data]
     rows = []
-    for row in parentData:
+    keyList = list(data[0].keys())
+    # with open("keylist.csv", "w") as keylst:
+    #     csv_w = csv.DictWriter(keylst,dialect='excel',delimiter=",", fieldnames=["key"])
+    #     csv_w.writeheader()
+    #     for key in keyList:
+    #         csv_w.writerow({"key":key})
+    for row in data:
         out=dict()
         #print("row: ", row) 
         for column in modelMeta.get('columns'):
+            if column["path"].find("@@List") > 0:
+                col_regex = re.escape(column["path"]).replace('@@List\.',"(\d\.)?")
+                r = re.compile(col_regex)
+                subset_columns = list(filter(r.match,keyList))
+                #print("KeyList: {} \n col_regex: {} Subset Columns: {}".format(keyList, col_regex,subset_columns))
+                values = []
+                for subset_column in subset_columns:
+                    values.append(row.get(subset_column))
+                out[column.get("name")] = values
+            else:
+                out[column.get("name")] = row.get(column.get("path"))
             #print("column: ", column)
-            out[column.get("name")] = row.get(cleanKey(column.get("path")))
+            # column_parts = column.split(".@@List.")
+            # column_parts_count = len(column_parts)
+            # counter = [0]*column_parts_count
+            
+            
+            #     
+           
+            #    if isinstance(dataset,list):
+                    
         #print("out: ", out)
         rows.append(out)
     
@@ -81,6 +108,7 @@ modelMeta = extractModelMeta(CONFIG,"connections")
 #print(f"SSIS Files: {ssis_files}")
 for dtsx_file in ssis_files:
     ssis_dict = xmlToDict(dtsx_file)
+    ssis_dict = dict(flatdict.FlatterDict(ssis_dict,delimiter = '.',))
     ssis_json_path = os.path.splitext(dtsx_file)[0] + ".json"
     with open(ssis_json_path,"w") as out_file:
         out_file.write(json.dumps(ssis_dict, indent=3))
